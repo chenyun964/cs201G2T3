6a74b722aeb59cd0c819ad05a6cce0c8f68352c8
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-10-21 18:28:00 +0800

    Update TestApplication.java

--- cs201/src/main/java/sg/edu/smu/app/TestApplication.java
+++ cs201/src/main/java/sg/edu/smu/app/TestApplication.java
@@ -1,11 +1,35 @@
 package sg.edu.smu.app;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class TestApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+//Creating the Frame
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 400);
 
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label1 = new JLabel("Enter id 1");
+        JTextField tf1 = new JTextField(20); // accepts upto 20 characters
+        JLabel label2 = new JLabel("Enter id 2");
+        JTextField tf2 = new JTextField(20); // accepts upto 20 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label1); // Components Added using Flow Layout
+        panel.add(tf1);
+        panel.add(label2); // Components Added using Flow Layout
+        panel.add(tf2);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.setVisible(true);
     }
 }
