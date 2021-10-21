ff0320522dbef9572ae1825bf9dd96562149294f
Chen Yun <chenyun964@gmail.com>
2021-10-21 18:30:58 +0800

    Test Case

--- cs201/src/main/java/sg/edu/smu/app/TestApplication.java
+++ cs201/src/main/java/sg/edu/smu/app/TestApplication.java
@@ -1,7 +1,28 @@
 package sg.edu.smu.app;
 
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        File file = new File("data.json");
+
+        try {
+            String content = FileUtils.readFileToString(file, "utf-8");
+            JSONObject tomJsonObject = new JSONObject(content);
+            JSONArray users = tomJsonObject.getJSONArray("users");
+
+            for (int i = 0; i < users.length(); i++) {
+                String user = (String) users.get(i);
+                System.out.println(user);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
