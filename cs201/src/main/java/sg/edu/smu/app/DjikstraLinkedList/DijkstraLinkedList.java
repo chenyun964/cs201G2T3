d7144d926c9f1f3e9fffefa3a6e62d567cb40f5a
gYunTian <54625060+gYunTian@users.noreply.github.com>
2021-10-26 23:33:06 +0800

    added simple sorted linked list

--- cs201/src/main/java/sg/edu/smu/app/DjikstraLinkedList/DijkstraLinkedList.java
+++ cs201/src/main/java/sg/edu/smu/app/DjikstraLinkedList/DijkstraLinkedList.java
@@ -1,6 +1,8 @@
 package sg.edu.smu.app.DjikstraLinkedList;
 
 import java.util.*;
+// javac -d ./build DijkstraLinkedList.java 
+// java -cp ./build DijkstraLinkedList
 
 public class DijkstraLinkedList {
 
@@ -12,12 +14,13 @@ public class DijkstraLinkedList {
 
   // each step find shortest path to a neighbour
   // repeat
-  
+
   private int numVertices;
   private Map<Integer, List<Node>> adjList;
   private int distArr[];
   private Set<Integer> visited;
   private PriorityQueue<Node> pq;
+  // private LinkedList<Node> ll;
 
   public DijkstraLinkedList(int numVertices) {
     this.numVertices = numVertices;
@@ -25,7 +28,7 @@ public class DijkstraLinkedList {
     this.visited = new HashSet<Integer>();
     this.pq = new PriorityQueue<Node>(numVertices, new Node());
   }
-  
+
   public static void main(String[] args) {
     //
     DijkstraLinkedList dji = new DijkstraLinkedList(9);
@@ -61,8 +64,10 @@ public class DijkstraLinkedList {
       if (pq.isEmpty())
         return;
 
+      // get the current node which has the shortest cost
       int u = pq.remove().node;
 
+      // if already visited next loop
       if (visited.contains(u))
         continue;
       visited.add(u);
@@ -71,13 +76,14 @@ public class DijkstraLinkedList {
       int edgeDistance = -1;
       int newDistance = -1;
 
+      // get the neighbours of the current lowest cost node
       for (int i = 0; i < adj.get(u).size(); i++) { // loop through neighbours of u
         Node v = adj.get(u).get(i); // get neighbour
 
         // If current node hasn't already been processed
         if (!visited.contains(v.node)) {
           edgeDistance = v.cost; // get distance from u to v
-          newDistance = distArr[u] + edgeDistance;
+          newDistance = distArr[u] + edgeDistance; // distance from current to neighbour plus neighbour cost
 
           // If new distance is cheaper in cost
           if (newDistance < distArr[v.node])
@@ -87,9 +93,7 @@ public class DijkstraLinkedList {
           pq.add(new Node(v.node, distArr[v.node]));
         }
       }
-
     }
-
   }
 
   static int getRandomNumber(int min, int max) {
@@ -99,66 +103,74 @@ public class DijkstraLinkedList {
   // Adjacency map
   static Map<Integer, List<Node>> getAdjList() {
     Map<Integer, List<Node>> adjList = new HashMap<Integer, List<Node>>();
-    Random rand = new Random();
 
     List<Node> l1 = new ArrayList<Node>();
-    l1.add(new Node(2, getRandomNumber(1,1)));
-    l1.add(new Node(3, getRandomNumber(1,1)));
-    l1.add(new Node(4, getRandomNumber(1,1)));
+    l1.add(new Node(2, getRandomNumber(1, 1)));
+    l1.add(new Node(3, getRandomNumber(1, 1)));
+    l1.add(new Node(4, getRandomNumber(1, 1)));
     adjList.put(1, l1);
 
     List<Node> l2 = new ArrayList<Node>();
-    l2.add(new Node(5, getRandomNumber(1,1)));
-    l2.add(new Node(6, getRandomNumber(1,1)));
-    l2.add(new Node(7, getRandomNumber(1,1)));
+    l2.add(new Node(5, getRandomNumber(1, 1)));
+    l2.add(new Node(6, getRandomNumber(1, 1)));
+    l2.add(new Node(7, getRandomNumber(1, 1)));
     adjList.put(2, l2);
 
     List<Node> l3 = new ArrayList<Node>();
-    l3.add(new Node(5, getRandomNumber(1,1)));
-    l3.add(new Node(6, getRandomNumber(1,1)));
-    l3.add(new Node(7, getRandomNumber(1,1)));
+    l3.add(new Node(5, getRandomNumber(1, 1)));
+    l3.add(new Node(6, getRandomNumber(1, 1)));
+    l3.add(new Node(7, getRandomNumber(1, 1)));
     adjList.put(3, l3);
 
     List<Node> l4 = new ArrayList<Node>();
-    l4.add(new Node(5, getRandomNumber(1,1)));
-    l4.add(new Node(6, getRandomNumber(1,1)));
-    l4.add(new Node(7, getRandomNumber(1,1)));
+    l4.add(new Node(5, getRandomNumber(1, 1)));
+    l4.add(new Node(6, getRandomNumber(1, 1)));
+    l4.add(new Node(7, getRandomNumber(1, 1)));
     adjList.put(4, l4);
 
     List<Node> l5 = new ArrayList<Node>();
-    l5.add(new Node(8, getRandomNumber(1,1)));
+    l5.add(new Node(8, getRandomNumber(1, 1)));
     adjList.put(5, l5);
 
     List<Node> l6 = new ArrayList<Node>();
-    l6.add(new Node(8, getRandomNumber(1,1)));
+    l6.add(new Node(8, getRandomNumber(1, 1)));
     adjList.put(6, l6);
 
     List<Node> l7 = new ArrayList<Node>();
-    l7.add(new Node(8, getRandomNumber(1,1)));
+    l7.add(new Node(8, getRandomNumber(1, 1)));
     adjList.put(7, l7);
 
     List<Node> l8 = new ArrayList<Node>();
     adjList.put(8, l8);
 
-
-    for (Map.Entry<Integer, List<Node>> entry : adjList.entrySet()) {
-      System.out.println("Key: " + entry.getKey());
-      List<Node> li = entry.getValue();
-      for (Node node : li) {
-        System.out.print(node.node + " : " + node.cost + " | ");
-      }
-      System.out.println("");
-    }
+    SortedLinkedList linkedList = new SortedLinkedList();
+    linkedList.add(new Node(1, 1));
+    linkedList.add(new Node(3, 3));
+    linkedList.add(new Node(0, 0));
+    linkedList.add(new Node(5, 5));
+    linkedList.add(new Node(2, 2));
+    linkedList.add(new Node(7, 7));
+    linkedList.add(new Node(8, 8));
+    System.out.println(linkedList.toString2());
+    System.out.println(linkedList.size());
+    linkedList.removeMin();
+    System.out.println(linkedList.toString());
+    System.out.println(linkedList.size());
 
     return adjList;
   }
 }
 
+/**
+ * Node class with public node, cost
+ */
 class Node implements Comparator<Node> {
 
   // Member variables of this class
   public int node;
   public int cost;
+  private Node prev;
+  private Node next;
 
   public Node() {
   }
@@ -172,10 +184,125 @@ class Node implements Comparator<Node> {
   public int compare(Node node1, Node node2) {
     if (node1.cost < node2.cost)
       return -1;
-
     if (node1.cost > node2.cost)
       return 1;
-
     return 0;
+  }
+
+  public Node getNext() {
+    return next;
+  }
+
+  public Node getPrev() {
+    return prev;
+  }
+
+  public void setNext(Node node) {
+    this.next = node;
+  }
+
+  public void setPrev(Node node) {
+    this.prev = node;
+  }
+
+}
+
+/**
+ * None Pointer doubly linked list
+ */
+class SortedLinkedList {
+  private Node header;
+  private Node trailer;
+  private int size = 0;
+
+  public SortedLinkedList() {
+    header = new Node(Integer.MAX_VALUE - 1, 0);
+    trailer = new Node(Integer.MAX_VALUE, Integer.MAX_VALUE);
+    trailer.setPrev(header);
+    header.setNext(trailer);
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * Insert node into LL and sort on insert
+   * 
+   * We can speed up by tracking largest elem and a pointer to the last smallest
+   * elem as well as every step, the last elem of that step
+   * 
+   * @param node
+   */
+  public void add(Node node) {
+    Node current = header.getNext();
+    // potentially o(n)
+    while (node.cost >= current.cost) { // loop until first elem that is greater than node
+      current = current.getNext();
+    }
+    current.getPrev().setNext(node);
+    node.setPrev(current.getPrev());
+    current.setPrev(node);
+    node.setNext(current);
+    this.size++;
+  }
+
+  /**
+   * Get size of linkedlist
+   * 
+   * @return Count
+   */
+  public int size() {
+    return this.size;
+  }
+
+  public Node min() {
+    if (isEmpty()) {
+      return null;
+    }
+    return header.getNext();
+  }
+
+  public Node max() {
+    if (isEmpty()) {
+      return null;
+    }
+    return trailer.getPrev();
+  }
+
+  public Node removeMin() {
+    if (isEmpty())
+      return null;
+
+    Node current = header.getNext();
+    header.setNext(current.getNext());
+    current.getNext().setPrev(header);
+    current.setNext(null);
+    current.setPrev(null);
+    this.size--;
+
+    return current;
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Node current = header;
+    while (current != trailer) {
+      sb.append(current.node);
+      sb.append(" ");
+      current = current.getNext();
+    }
+    return sb.toString();
+  }
+
+  public String toString2() {
+    StringBuilder sb = new StringBuilder();
+    Node current = trailer;
+    while (current != header) {
+      sb.append(current.node);
+      sb.append(" ");
+      current = current.getPrev();
+    }
+    return sb.toString();
   }
 }
