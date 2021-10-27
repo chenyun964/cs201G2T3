d9aec83e656ee310d31dd6c649fee38a76276244
Chen Yun <chenyun964@gmail.com>
2021-10-27 21:28:22 +0800

    fix merge issue

--- cs201/src/main/java/sg/edu/smu/app/TestApplication.java
+++ cs201/src/main/java/sg/edu/smu/app/TestApplication.java
@@ -71,67 +71,17 @@ public class TestApplication {
         frame.setVisible(true);
     }
 
+    public static void main(String[] args) {
+        TestApplication app = new TestApplication();
+        app.createUI();
 
-    }
-}
-
-        long endTime = System.nanoTime();
-        long totalTime = endTime - startTime;
-        System.out.println(totalTime / 1000000000.0 + " seconds");
-
-        // System.out.println(g);
-    }
-
-class edge {
-    int i, j;
-
-    public edge(int ii, int jj) {
-        i = ii;
-        j = jj;
-    }
-
-    }
-
-    public void addNode(String name, int x, int y) {
-        // add a node at pixel (x,y)
-        nodes.add(new Node(name, x, y));
-        this.repaint();
-    }
-
-    public void addEdge(int i, int j) {
-        // add an edge between nodes i and j
-        edges.add(new edge(i, j));
-        this.repaint();
-    }
-
-    public void paint(Graphics g) { // draw the nodes and edges
-        FontMetrics f = g.getFontMetrics();
-        int nodeHeight = Math.max(height, f.getHeight());
-
-        g.setColor(Color.black);
-        for (edge e : edges) {
-            g.drawLine(nodes.get(e.i).x, nodes.get(e.i).y, nodes.get(e.j).x, nodes.get(e.j).y);
-        }
-
-        for (Node n : nodes) {
-            int nodeWidth = Math.max(width, f.stringWidth(n.name) + width / 2);
-            g.setColor(Color.white);
-            g.fillOval(n.x - nodeWidth / 2, n.y - nodeHeight / 2, nodeWidth, nodeHeight);
-            g.setColor(Color.black);
-            g.drawOval(n.x - nodeWidth / 2, n.y - nodeHeight / 2, nodeWidth, nodeHeight);
-
-            g.drawString(n.name, n.x - f.stringWidth(n.name) / 2, n.y + f.getHeight() / 2);
-        }
-    }
-
-    public static Graph<String, Integer> generateAdjacencyMapGraphFromData() {
         JSONParser parser = new JSONParser();
 
-        // By Default, the dataset is undirected by default
         Graph<String, Integer> g = new AdjacencyMapGraph<>(false);
         TreeSet<String> labels = new TreeSet<>();
         HashMap<String, Vertex<String>> verts = new HashMap<>();
 
+        long startTime = System.nanoTime();
         try (Reader reader = new FileReader("data.json")) {
             JSONArray users = (JSONArray) parser.parse(reader);
             for (Object u : users) {
@@ -142,11 +92,13 @@ class edge {
                 String[] friends = friendString.replace(" ", "").split(",");
                 for (String s : friends) {
                     labels.add(s);
+
                 }
             }
             for (String label : labels) {
                 verts.put(label, g.insertVertex(label));
             }
+
             for (Object u : users) {
                 JSONObject user = (JSONObject) u;
                 String user_id = (String) user.get("user_id");
@@ -162,30 +114,10 @@ class edge {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return g;
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime / 1000000000.0 + " seconds");
+
+        // System.out.println(g);
     }
-
-    public static Graph<String, Integer> graphFromEdgelist(String[][] edges, boolean directed) {
-        Graph<String, Integer> g = new AdjacencyMapGraph<>(directed);
-
-        // first pass to get sorted set of vertex labels
-        TreeSet<String> labels = new TreeSet<>();
-        for (String[] edge : edges) {
-            labels.add(edge[0]);
-            labels.add(edge[1]);
-        }
-
-        // now create vertices (in alphabetical order)
-        HashMap<String, Vertex<String>> verts = new HashMap<>();
-        for (String label : labels)
-            verts.put(label, g.insertVertex(label));
-
-        // now add edges to the graph
-        for (String[] edge : edges) {
-            Integer cost = (edge.length == 2 ? 1 : Integer.parseInt(edge[2]));
-            g.insertEdge(verts.get(edge[0]), verts.get(edge[1]), cost);
-        }
-        return g;
-    }
-
 }
