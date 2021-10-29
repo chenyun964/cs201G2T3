6b4c02951c690fec5f02fb41b5d024ad1c559a46
gYunTian <54625060+gYunTian@users.noreply.github.com>
2021-10-30 01:26:20 +0800

    minor change

--- cs201/src/main/java/sg/edu/smu/app/experiments/RunDJI.java
+++ cs201/src/main/java/sg/edu/smu/app/experiments/RunDJI.java
@@ -8,10 +8,12 @@ public class RunDJI {
   private static final long MEGABYTE = 1024L * 1024L;
   private static final Double divider = 1000000000.0;
   private static long startTime;
-  private static  long endTime;
-  private static  long totalTime;
+  private static long endTime;
+  private static long totalTime;
   private static long memory;
-  public RunDJI() {}
+
+  public RunDJI() {
+  }
 
   public static void main(String[] args) {
   }
@@ -22,10 +24,10 @@ public class RunDJI {
     System.out.println("Adjacency Map + Djikstra Sorted PQ");
     startTime = System.nanoTime();
     runtime = Runtime.getRuntime();
-    
+
     DijkstraLinkedList dji = new DijkstraLinkedList(numVertices);
     dji.dijkstra_PQ(adjMap, id1);
-    System.out.println("Shortest path length is: " +  dji.distArr[id2]);
+    System.out.println("Shortest path length is: " + dji.distArr[id2]);
     endTime = System.nanoTime();
     totalTime = endTime - startTime;
     System.out.println();
@@ -63,27 +65,31 @@ public class RunDJI {
   }
 
   public void runLinearlySortedLinkedList(int numVertices, Map<Integer, List<CustomNode>> adjMap, int id1, int id2) {
-        // startTime = System.nanoTime();
-        // runtime = Runtime.getRuntime();
-        // dji = new DijkstraLinkedList(numVertices);
-        // dji.dijkstra_LL(adjMap, id1);
-        // System.out.println("Shortest path length is: " + dji.distArr[id2]);
-        // endTime = System.nanoTime();
-        // totalTime = endTime - startTime;
-        // System.out.println();
-        // System.out.println("Time to Compute Path: " + totalTime / divider + "s");
-        // // Run the garbage collector
-        // runtime.gc();
-        // // Calculate the used memory
-        // memory = runtime.totalMemory() - runtime.freeMemory();
-        // System.out.println("Used memory is bytes: " + memory);
-        // System.out.println("Used memory is megabytes: " + bytesToMegabytes(memory));
-        System.out.println("This actually takes very long, hence pre run results are printed instead:");
-        System.out.println("Shortest path length is: 5");
-        System.out.println("Time to Compute Path: 4774.6261447s");
-        System.out.println("Used memory is bytes: 134289280");
-        System.out.println("Used memory is megabytes: 128");
-        System.out.println("\n--------------------------------------------------\n");
+    // Runtime runtime = Runtime.getRuntime();
+
+    // DijkstraLinkedList dji = new DijkstraLinkedList(numVertices);
+    // System.out.println("Adjacency Map + Djikstra (Sorted LL w/o HashMap, like simple linear sorted array)");
+    // startTime = System.nanoTime();
+    // runtime = Runtime.getRuntime();
+    // dji = new DijkstraLinkedList(numVertices);
+    // dji.dijkstra_LL(adjMap, id1);
+    // System.out.println("Shortest path length is: " + dji.distArr[id2]);
+    // endTime = System.nanoTime();
+    // totalTime = endTime - startTime;
+    // System.out.println();
+    // System.out.println("Time to Compute Path: " + totalTime / divider + "s");
+    // // Run the garbage collector
+    // runtime.gc();
+    // // Calculate the used memory
+    // memory = runtime.totalMemory() - runtime.freeMemory();
+    // System.out.println("Used memory is bytes: " + memory);
+    // System.out.println("Used memory is megabytes: " + bytesToMegabytes(memory));
+    System.out.println("This actually takes very long, hence pre run results are printed instead");
+    System.out.println("Shortest path length is: 5");
+    System.out.println("Time to Compute Path: 4774.6261447s");
+    System.out.println("Used memory is bytes: 134289280");
+    System.out.println("Used memory is megabytes: 128");
+    System.out.println("\n--------------------------------------------------\n");
   }
 
   public void runMinStack(int numVertices, Map<Integer, List<CustomNode>> adjMap, int id1, int id2) {
@@ -111,7 +117,7 @@ public class RunDJI {
 
   public void runHashMapCircular(int numVertices, Map<Integer, List<CustomNode>> adjMap, int id1, int id2) {
     Runtime runtime = Runtime.getRuntime();
-    
+
     DijkstraLinkedList dji = new DijkstraLinkedList(numVertices);
     System.out.println("Adjacency Map + Djikstra (HashMap w Circular Array as Queue)");
     startTime = System.nanoTime();
@@ -131,7 +137,6 @@ public class RunDJI {
     System.out.println("Used memory is megabytes: " + bytesToMegabytes(memory));
     System.out.println("\n--------------------------------------------------\n");
   }
-
 
   public static long bytesToMegabytes(long bytes) {
     return bytes / MEGABYTE;
