dd353cff8077fc5e6f13325e6a818be3065bf910
Chen Yun <chenyun964@gmail.com>
2021-11-02 22:14:09 +0800

    last update

--- cs201/src/main/java/sg/edu/smu/app/TestApplication.java
+++
@@ -1,8 +0,0 @@
-package sg.edu.smu.app;
-
-public class TestApplication {
-    public static void main(String[] args) {
-        UserInterface app = new UserInterface();
-        app.createUI();
-    }
-}
